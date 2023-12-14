fd2a047efe68d301dcd586c148e91f8574411b1f
efe33 <efe33@BPC04>
2023-12-14 14:53:09 +0300

    Proje dosyası ekle.

---
+++ Kalitim/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalitim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Ogretmen ogr = new Ogretmen
+                {
+                    Ad = "EFE KAAN ",
+                    Soyad = "KAZGAN ",
+                    Brans = "Matematik",
+ };
+                OkulPersoneli per = ogr; 
+                Console.WriteLine(per.Ad);
+                Console.WriteLine(per.Soyad);
+                
+
+            }
+        }
+    }
+    }
+    public class OkulPersoneli
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+    }
+    public class Ogretmen : OkulPersoneli
+    {
+        public string Brans { get; set; }
+    }
+
